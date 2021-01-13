2c45f21f01d5f9883368a35bbdb443cd31633615
nitin-codr <nitinporwal99@gmail.com>
2021-01-13 15:34:09 +0530

    add media player ui

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+    }
+
+}
